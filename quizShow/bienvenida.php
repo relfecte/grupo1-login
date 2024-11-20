0520e158dc7aa3bf284215d8a1b8b6c5aca03be0
relfecte <aleix19196@gmail.com>
2024-11-20 09:42:27 +0100

    quizShow es la web

---
+++ quizShow/bienvenida.php
@@ -0,0 +1,27 @@
+<?php 
+session_start();
+if(!isset($_SESSION['email']) && $_SESSION['admin']== 0){
+    echo'
+        <script>
+            alert("Por favor debes iniciar sesión");
+            window.location = "index.php";
+        </script>';
+
+    session_destroy();
+    die();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenidos </title>
+    
+</head>
+<body>
+    <h1>Bienvenido a mi mundo <?php echo $_SESSION["usuario"]?></h1>
+    <a href="./inc/logout.php">Cerrar sesión</a>
+</body>
+</html>
