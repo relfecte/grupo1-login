ceba7eaecf8a39760a1b5b44fb0c6b7504439af5
OscarCR94 <chavesreyeso@gmail.com>
2024-11-24 13:37:26 +0100

    Update bienvenida.php

--- admin_usuarios/bienvenida.php
+++ admin_usuarios/bienvenida.php
@@ -17,11 +17,20 @@ if(!isset($_SESSION['email']) && $_SESSION['admin']== 0){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenidos </title>
+    <link rel="stylesheet" href="./css/style.css">
+
+
+    <title>Bienvenido </title>
     
 </head>
 <body>
-    <h1>Bienvenido a mi mundo <?php echo $_SESSION["usuario"]?></h1>
+     <header>
+        <h1 class="main-title">Bienvenido a QuizShow</h1>
+        <h2 class="subtitle">Bienvenido</h2>
+        <p class="description">Selecciona una opción para comenzar:</p>
+    </header>
+
     <a href="./inc/logout.php">Cerrar sesión</a>
+    <script src="./js/script.js"></script>
 </body>
 </html>
