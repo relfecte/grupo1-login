ce07a5170f164bd825921d530f51e73bc892ec61
OscarCR94 <chavesreyeso@gmail.com>
2024-12-07 11:02:19 +0100

    Update direcciones bienvenida.php

--- quizShow/bienvenida.php
+++ quizShow/bienvenida.php
@@ -17,21 +17,21 @@ if(!isset($_SESSION['email']) && $_SESSION['admin']== 0){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/estilo.css">
+    <link rel="stylesheet" href="../css/estilo.css">
     <title>Game Quiz</title>
 </head>
 <body>
     <div class="container">
-        <img src="assets/img1/auris.png" alt="" class="img">
+        <img src="../img/auris.png" alt="" class="img">
         <header>
             <div class="puntaje">
-                <img src="assets/img1/puntos.png" alt="" >
+                <img src="../img/puntos.png" alt="" >
                 <span class="puntos" id="puntos">0</span>
             </div>
             <h1>QUIZ</h1>
             <div class="jugador">
                 <span class="nombre" id="nombre"></span>
-                <img src="assets/img1/user.png" alt="">
+                <img src="../img/user.png" alt="">
                 </div>
         </header>
 
@@ -41,41 +41,41 @@ if(!isset($_SESSION['email']) && $_SESSION['admin']== 0){
             <p>Elige una categoría..</p>
             <div class="categorias">
                 <div class="categoria" id="general">
-                    <img src="assets/img1/general.png" alt="" class="foto-categoria">
+                    <img src="../img/general.png" alt="" class="foto-categoria">
                     <h3>General</h3>
                 </div>
                 <div class="categoria" id="musica">
-                    <img src="assets/img1/musica.png" alt="" class="foto-categoria">
+                    <img src="../img/musica.png" alt="" class="foto-categoria">
                     <h3>Música</h3>
                 </div>
                 <div class="categoria" id="peliculas">
-                    <img src="assets/img1/peliculas.png" alt="" class="foto-categoria">
+                    <img src="../img/peliculas.png" alt="" class="foto-categoria">
                     <h3>Películas</h3>
                 </div>
                 <div class="categoria" id="deportes">
-                    <img src="assets/img1/deportes.png" alt="" class="foto-categoria">
+                    <img src="../img/deportes.png" alt="" class="foto-categoria">
                     <h3>Deportes</h3>
                 </div>
                 <div class="categoria" id="programacion">
-                    <img src="assets/img1/programacion.png" alt="" class="foto-categoria">
+                    <img src="../img/programacion.png" alt="" class="foto-categoria">
                     <h3>Programación</h3>
                 </div>
                 <div class="categoria" id="juegos">
-                    <img src="assets/img1/juegos.png" alt="" class="foto-categoria">
+                    <img src="../img/juegos.png" alt="" class="foto-categoria">
                     <h3>Video Juegos</h3>
                 </div>
                 <div class="categoria" id="geografia">
-                    <img src="assets/img1/Geografia.jfif" alt="" class="foto-categoria1">
+                    <img src="../img/Geografia.jfif" alt="" class="foto-categoria1">
                     <h3>Geografía</h3>
                 </div>
                 <div class="categoria" id="juegos">
-                    <img src="assets/img1/Historia.jfif" alt="" class="foto-categoria1">
+                    <img src="../img/Historia.jfif" alt="" class="foto-categoria1">
                     <h3>Historia</h3>
                 </div>
                 </div>
         </main>
     </div>
 
-    <script src="./js/script.js"></script>
+    <script src="../js/script.js"></script>
 </body>
 </html>
