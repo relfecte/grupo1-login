0aa0b96af713de792062d5bd4bebcef9ec32c234
OscarCR94 <chavesreyeso@gmail.com>
2025-02-12 10:24:36 +0100

    Update registro.php
    Cambio de clases en el registro, dejando el php existente

--- vistas/registro.php
+++ vistas/registro.php
@@ -8,22 +8,26 @@ $conexion = $con;
 verificarAccesoUsuario2();
 
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Puntuaciones del Quiz</title>
-    <link rel="stylesheet" href="../css/estilo.css">
+    <link rel="stylesheet" href="../css/registro.css">
+    
+
+    
 </head>
 <body>
-    <div class="container__register">
-        <header class="header__register">
+    <div class="container__registro">
+        <header class="header__registro">
             Puntuaciones del Quiz
         </header>
-        <div class="general-score">
+        <div class="general-score__registro">
             <h2>Puntaje General</h2>
-            <span id="puntaje-general" class="percentage">0</span> <!-- Aquí se actualizará el puntaje general -->
+            <span id="puntaje-general" class="percentage__registro">0</span>
         </div>
         <script>
             // Asignamos el valor del porcentaje calculado desde PHP
@@ -35,16 +39,18 @@ verificarAccesoUsuario2();
             // Mostrar el porcentaje en el elemento HTML
             txtPuntaje.innerHTML = porcentajeTotal + '%';
         </script>
-        <div class="scores">
+        <div class="scores__registro">
             <h3>Puntajes por Categoría</h3>
             <div id="categorias-container">
                 <?php generarCategorias($conexion, $_SESSION['usuario_id']); ?>
             </div>
         </div>
-
-        <div style="text-align: center; margin-top: 20px;">
-            <button onclick="window.location.href='../bienvenida.php'" class="btn">Volver a inicio</button>
+        <div style="text-align: center; margin-top: 10px;">
+            <button onclick="window.location.href='principal.html'" class="btn__registro">Volver a inicio</button>
         </div>
     </div>
+    <script src="../js/registro.js"></script>
 </body>
 </html>
+
+
