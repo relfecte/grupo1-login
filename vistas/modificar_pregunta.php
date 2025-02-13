959668f19b858c2db2554dcd63e2636ed1282cb8
OscarCR94 <chavesreyeso@gmail.com>
2025-02-13 13:53:59 +0100

    Update modificar_pregunta.php

--- vistas/modificar_pregunta.php
+++ vistas/modificar_pregunta.php
@@ -5,31 +5,44 @@ $pregunta_id = $_GET['pregunta_id'];  // Obtener el ID de la pregunta
 // Obtener datos de la pregunta
 $pregunta = obtenerPreguntaPorID($con, $pregunta_id);
 ?>
-
-<form class="form-container" action="../inc/admin/modificar_pregunta.php?pregunta_id=<?php echo $pregunta_id; ?>" method="POST">
-    <label>Categoría:</label>
-    <input type="text" name="categoria" value="<?php echo $pregunta['categoria']; ?>" required pattern="[a-zA-Z0-9\s]{3,50}" maxlength="50">
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario Admin</title>
+    <link rel="stylesheet" href="../css/admin_estilos.css">
+</head>
+<body>
+<form class="formulario-admin" action="../inc/admin/modificar_pregunta.php?pregunta_id=<?php echo $pregunta_id; ?>" method="POST">
+    <div class="header-admin">Modificar pregunta</div>
+    <label class="label-admin">Categoría:</label>
+    <input class="input-admin" type="text" name="categoria" value="<?php echo $pregunta['categoria']; ?>" required pattern="[a-zA-Z0-9\s]{3,50}" maxlength="50">
     <br>
 
-    <label>Pregunta:</label>
-    <textarea name="pregunta" required><?php echo $pregunta['pregunta']; ?></textarea>
+    <label class="label-admin">Pregunta:</label>
+    <textarea class="textarea-admin" name="pregunta" required><?php echo $pregunta['pregunta']; ?></textarea>
     <br>
 
-    <label>Opción Correcta:</label>
-    <input type="text" name="opcion_correcta" value="<?php echo $pregunta['opcion_correcta']; ?>" required>
+    <label class="label-admin">Opción Correcta:</label>
+    <input class="input-admin" type="text" name="opcion_correcta" value="<?php echo $pregunta['opcion_correcta']; ?>" required>
     <br>
 
-    <label>Opción 2:</label>
-    <input type="text" name="opcion2" value="<?php echo $pregunta['opcion2']; ?>" required>
+    <label class="label-admin">Opción 2:</label>
+    <input class="input-admin" type="text" name="opcion2" value="<?php echo $pregunta['opcion2']; ?>" required>
     <br>
 
-    <label>Opción 3:</label>
-    <input type="text" name="opcion3" value="<?php echo $pregunta['opcion3']; ?>" required>
+    <label class="label-admin">Opción 3:</label>
+    <input class="input-admin" type="text" name="opcion3" value="<?php echo $pregunta['opcion3']; ?>" required>
     <br>
 
-    <label>Opción 4:</label>
-    <input type="text" name="opcion4" value="<?php echo $pregunta['opcion4']; ?>" required>
+    <label class="label-admin">Opción 4:</label>
+    <input class="input-admin" type="text" name="opcion4" value="<?php echo $pregunta['opcion4']; ?>" required>
     <br>
 
-    <button type="submit">Modificar Pregunta</button>
+    <button class="button-admin" type="submit">Modificar Pregunta</button>
 </form>
+
+<script src="../js/crear-usuario.js"></script>
+</body>
+</html>
